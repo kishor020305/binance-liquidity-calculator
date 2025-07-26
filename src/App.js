faedf00c16d8581fdcc34696807a219a3bc08ea5
kishor020305 <140971934+kishor020305@users.noreply.github.com>
2025-07-26 22:56:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LiquidationCalculator from "./components/LiquidationCalculator";
+
+function App() {
+  return (
+    <div className="App">
+      <LiquidationCalculator />
+    </div>
+  );
+}
+
+export default App;
